8dfde876e4261d2ae90ee8ba82c8cde7c2e9d260
Mihaela Lisman <78301504+MihaelaLisman@users.noreply.github.com>
2024-01-26 18:20:50 +0200

    Add files via upload

--- AllBrandsListAPI/tests/test_get_brands_no_in_list.py
+++ AllBrandsListAPI/tests/test_get_brands_no_in_list.py
@@ -1,12 +1,12 @@
 import unittest
 
-from AllBrandsListAPI.requests.all_brands_list_requests import get_status_all_brands_list, get_brands_no_in_list
+from AllBrandsListAPI.requests.all_brands_list_requests import get_request
 
 
 class TestAPIallBrands(unittest.TestCase):
 
     def test_get_brands_no_in_list(self):
-        response = get_brands_no_in_list()
+        response = get_request()
         brands = response["brands"]
         expected_brands_no = 34
         actual_brands_no = len(brands)
--- AllBrandsListAPI/tests/test_get_status_all_brands_list.py
+++ AllBrandsListAPI/tests/test_get_status_all_brands_list.py
@@ -1,12 +1,12 @@
 import unittest
 
-from AllBrandsListAPI.requests.all_brands_list_requests import get_status_all_brands_list, get_brands_no_in_list
+from AllBrandsListAPI.requests.all_brands_list_requests import get_request
 
 
 class TestAPIallBrands(unittest.TestCase):
 
     def test_get_status_all_brands_list(self):
-        response = get_status_all_brands_list()
+        response = get_request()
         expected_response = 200
         actual_response = response["responseCode"]
         assert actual_response == expected_response, "The status is not correct."
